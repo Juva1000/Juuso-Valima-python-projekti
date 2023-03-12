808dafaa1c0d20f9d8876c597cd0709413a2d12c
Juva1000 <125983014+Juva1000@users.noreply.github.com>
2023-03-12 23:41:47 +0200

    Update ostoslista.py

--- ostoslista.py
+++ ostoslista.py
@@ -1,94 +1,113 @@
 import json
 import datetime
+from collections import defaultdict
+
+json_tiedosto_kt = "kt.json"
+json_tiedosto_kulut = "kulut.json"
 
 
-data = {}
-with open("ostokset.json", "w") as f:
-    json.dump(data, f)
 
-
-def main():
-    while True:
-        print("Tervetuloa sovellukseeni")
-        print("Valitse seuraavista:")
-        print("1. Lisää ostos")
-        print("2. Muokkaa ostoksia")
-        print("3. Lopeta")
-        valinta = input("Valintasi: ")
-
-        if valinta == "1":
-            lisaa_ostos()
-        elif valinta == "2":
-            muokkaa_ostoksia()
-        elif valinta == "3":
-            print("Kiitos ohjelman käytöstä!")
-            break
-        else:
-            print("Virheellinen valinta, yritä uudelleen.")
-
-
-def lisaa_ostos():
-    print("Lisää ostos alkaa")
-    kayttajatunnus = input("Anna käyttäjätunnus: ")
-    kauppa = input("Kauppa: ")
-    hinta = input("Hinta: ")
-    time = datetime.datetime.now().strftime("%H:%M")
-    date = datetime.datetime.now().strftime("%d.%m.%Y")
-
-    item_dict = {"kauppa": kauppa, "hinta": hinta, "aika": time, "paiva": date}
-    with open("ostokset.json", "r+") as f:
-        data = json.load(f)
-        if kayttajatunnus not in data:
-            data[kayttajatunnus] = []
-        data[kayttajatunnus].append(item_dict)
-        f.seek(0)
-        json.dump(data, f, indent=4)
-        f.truncate()
-    print("Ostos tallennettu")
-
-
-def muokkaa_ostoksia():
-    print("Muokkaa ostoksia alkaa")
-    kayttajatunnus = input("Anna käyttäjätunnus: ")
-    with open("ostokset.json", "r") as f:
-        data = json.load(f)
-    ostokset = data.get(kayttajatunnus, [])
-
-    for i, ostos in enumerate(ostokset):
-        print(f"{i+1}. Kauppa: {ostos['kauppa']}, Hinta: {ostos['hinta']}, Aika: {ostos['aika']}, Päivä: {ostos['paiva']}")
+def lataa_kt():
+    try:
+        with open (json_tiedosto_kt) as tiedosto_kt:
+            return json.load(tiedosto_kt)
+    except FileNotFoundError:
+        return[]
     
-    if not ostokset:
-        print("Ei ostoksia tälle käyttäjälle")
-        return
+
+def tallenna_kt(tallenna_kt):
+    with open(json_tiedosto_kt, "w") as tiedosto_kt:
+        json.dump(tallenna_kt, tiedosto_kt)
+
+
+def lataa_kulut():
+    try:
+        with open(json_tiedosto_kulut) as tiedosto_kulut:
+            return json.load(tiedosto_kulut)
+    except FileNotFoundError:
+        return []
     
-    while True:
-        valinta = input("Valitse muokattava ostos tai paina 'q' poistuaksesi: ")
-        if valinta == 'q':
-            break
-        elif not valinta.isnumeric():
-            print("Virheellinen syöte, yritä uudelleen.")
-            continue
-        valinta = int(valinta)
-        if valinta < 1 or valinta > len(ostokset):
-            print("Virheellinen syöte, yritä uudelleen.")
-            continue
-        
-        
-        ostos = ostokset[valinta-1]
-        print(f"Muokataan ostosta: {ostos['kauppa']}, {ostos['hinta']}, {ostos['aika']}, {ostos['paiva']}")
-        uusi_kauppa = input("Anna uusi kauppa tai jätä tyhjäksi jos ei muutosta: ")
-        if uusi_kauppa:
-            ostos['kauppa'] = uusi_kauppa
-        uusi_hinta = input("Anna uusi hinta tai jätä tyhjäksi jos ei muutosta: ")
-        if uusi_hinta:
-            ostos['hinta'] = uusi_hinta
-        
-        
-        with open("ostokset.json", "w") as f:
-            json.dump(data, f, indent=4)
-        
-        print("Ostos muokattu")
+
+def tallenna_kulut(tallenna_kulut):
+    with open(json_tiedosto_kulut, "w") as tiedosto_kulut:
+        json.dump(tallenna_kulut, tiedosto_kulut)    
 
 
-if __name__ == "__main__":
-    main()
+def kirjaudu_sisaan(kayttajat, tunnus, salasana):
+    for kayttaja in kayttajat:
+        if kayttaja["tunnus"] == tunnus and kayttaja["salasana"] == salasana:
+            print("Kirjautuminen onnistui!")
+            return kayttaja["tunnus"]
+    print("Virheellinen valinta.")
+    return None
+
+
+def laske_kulut_yhteensa(kulut):
+    kulut_yhteensa = defaultdict(int)
+    for kulu in kulut:
+        kayttaja = kulu["kayttaja"]
+        hinta = int(kulu["hinta"])
+        kulut_yhteensa[kayttaja] += hinta
+    return kulut_yhteensa
+
+kayttajat = lataa_kt()
+kulut = lataa_kulut()
+
+
+while True:
+    print("1. Kirjaudu sisään")
+    print("2. Luo käyttäjätunnus")
+    valinta = input("Valitse toiminto: ")
+
+    if valinta == "1":
+        tunnus = input("Anna käyttäjätunnus:")
+        salasana = input("Anna salasana: ")
+        kirjautunut_kayttaja = kirjaudu_sisaan(kayttajat, tunnus, salasana)
+        if kirjautunut_kayttaja:
+            print("Tervetuloa takaisin " + tunnus + "!")
+            while True:
+                print("1. Syötä merkintä")
+                print("2. Näytä lista")
+                print("3. Kirjaudu ulos")
+                print("4. Laske kulut yhteensä")
+                valinta = input("Valitse toiminto: ")
+                if valinta == "1":
+                    kauppa = input("Kauppa: ")
+                    hinta = input("Hinta: ")
+                    pvm = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    kulut.append({"kauppa": kauppa, "hinta": hinta, "pvm": pvm, "kayttaja": kirjautunut_kayttaja})
+                    tallenna_kulut(kulut)
+                    
+                elif valinta == "2":
+                    for kulu in kulut:
+                        if kulu["kayttaja"] == kirjautunut_kayttaja:
+                            kauppa = kulu["kauppa"]
+                            hinta = kulu["hinta"]
+                            pvm = kulu["pvm"]
+                            print(pvm + ": " + kauppa + ", " + hinta + "€")
+
+                elif valinta == "3":
+                    print("Kirjaudutaan ulos...")
+                    break
+
+                elif valinta == "4":
+                    kulut_yhteensa = laske_kulut_yhteensa(kulut)
+                    for kayttaja, summa in kulut_yhteensa.items():
+                        print(f"{kayttaja}, {summa}€")
+
+                else:
+                    print("Valitse 1, 2 tai 3.")
+
+
+                   
+    elif valinta == "2":
+        uusi_tunnus = input("Anna uusi käyttäjätunnus: ")
+        uusi_salasana = input("Anna uusi salasana: ")
+        uusi_kayttaja = {"tunnus": uusi_tunnus, "salasana": uusi_salasana}
+        kayttajat.append(uusi_kayttaja)
+        tallenna_kt(kayttajat)
+        print("Käyttäjätunnus lisätty onnistuneesti")
+
+
+
+
